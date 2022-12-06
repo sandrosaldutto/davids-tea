e10503a9ee5d63d2f1bbea0e0b3c4094e4af0062
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-12-06 09:39:27 -0700

    added wirefram screenshots

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Persona from "./components/Persona/Persona";
+import Wires from "./components/Wires/Wires";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Persona />
+        <Wires />
       </BrowserRouter>
     </>
   );
